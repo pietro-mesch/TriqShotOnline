1e0e0d2cd09824062fd73de2e313841a7fb4d3f9
pietro-mesch <mezkaster@gmail.com>
2022-11-27 00:42:08 +0100

    got the animation running set viewport size somehow (?)

---
+++ generateLevel.js
@@ -0,0 +1,22 @@
+const vp = document.getElementById('viewPort');
+const gv = document.getElementById('gameView');
+const VP_WIDTH = vp.width = window.innerWidth * 0.8;
+const VP_HEIGHT = vp.height = VP_WIDTH * 3 / 4;
+gv.width = VP_WIDTH;
+gv.height = VP_HEIGHT;
+
+const ctx = gv.getContext('2d');
+let x = 0;
+
+function generateLevel() {
+    ctx.clearRect(0, 0, VP_WIDTH, VP_HEIGHT);
+    ctx.fillStyle = "#32cd32";
+    ctx.fillRect(x, 10, 50, 50);
+    x++;
+    if (x > VP_WIDTH) {
+        x = 0;
+    }
+    requestAnimationFrame(generateLevel);
+}
+
+// generateLevel();
---
+++ setDimensions.js
@@ -0,0 +1,10 @@
+function setDimensions() {
+    // var viewPort = document.getElementById("viewPort");
+    // viewPort.width = 300;
+    // viewPort.heigth = 300;
+    // var gameView = document.getElementById("gameView");
+    // gameView.width = 300;
+    // gameView.heigth = 300;
+}
+
+setDimensions();
