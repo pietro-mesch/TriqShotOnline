33f0c9be7cedd8460e1ff3861f5522703b9e1eb2
pietro-mesch <mezkaster@gmail.com>
2022-11-27 02:19:11 +0100

    planet generation

--- generateLevel.js
+++ generateLevel.js
@@ -2,19 +2,37 @@ const vp = document.getElementById('viewPort');
 const gv = document.getElementById('gameView');
 const VP_WIDTH = vp.width = window.innerWidth * 0.8;
 const VP_HEIGHT = vp.height = VP_WIDTH * 3 / 4;
-// gv.width = VP_WIDTH;
-// gv.height = VP_HEIGHT;
 
 const ctx = gv.getContext('2d');
 
+const PLANET_COLOUR = "#32cd32";
+const PLANET_THICKNESS = 3;
+const PLANET_MIN_RADIUS = 25;
+const PLANET_MAX_RADIUS = 150;
+
+class Planet {
+    constructor(fieldWidth, fieldHeigth) {
+        this.x = Math.floor(Math.random() * (fieldWidth + 1));
+        this.y = Math.floor(Math.random() * (fieldHeigth + 1));
+        this.radius = Math.floor(Math.random() * (PLANET_MAX_RADIUS - PLANET_MIN_RADIUS + 1)) + PLANET_MIN_RADIUS;
+        this.mass = this.radius ^ 3 * Math.PI * 4 / 3;
+    }
+}
+
+
 function generateLevel() {
     ctx.clearRect(0, 0, VP_WIDTH, VP_HEIGHT);
-    ctx.fillStyle = "#32cd32";
-
-    x = Math.floor(Math.random() * (VP_WIDTH + 1));
-    y = Math.floor(Math.random() * (VP_HEIGHT + 1));
-
-    ctx.fillRect(x, y, 50, 50);
+    
+    p = new Planet(VP_WIDTH, VP_HEIGHT);
+    drawPlanet(p);
 
     requestAnimationFrame();
+}
+
+function drawPlanet(p) {
+    ctx.strokeStyle = PLANET_COLOUR;
+    ctx.lineWidth = PLANET_THICKNESS;
+    ctx.beginPath();
+    ctx.ellipse(p.x, p.y, p.radius, p.radius, 0, 0, 2 * Math.PI);
+    ctx.stroke();
 }
